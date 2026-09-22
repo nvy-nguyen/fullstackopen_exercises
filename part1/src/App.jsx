9e59e2d1251110279732e1b7d5b0ecc048f1fd27
nvy-nguyen <prettyopportunist@gmail.com>
2026-09-22 14:34:07 +0900

    Submit 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -29,27 +29,29 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const partsarray = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    partsarray: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content parts={partsarray} />
-      <Total parts={partsarray} />
+      <Header course={course.name} />
+      <Content parts={course.partsarray} />
+      <Total parts={course.partsarray} />
     </div>
   )
 }
