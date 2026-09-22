b7b434a6c5a7b54cab929689fe140ffbba4edc43
nvy-nguyen <prettyopportunist@gmail.com>
2026-09-22 13:07:43 +0900

    Submit 1.3

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -46,9 +46,13 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part1={part1.name} exercises1={part1.exercises} />
-      <Content part2={part2.name} exercises2={part2.exercises} />
-      <Content part3={part3.name} exercises3={part3.exercises} />
+      <Content part1={part1.name} 
+        exercises1={part1.exercises} 
+        part2={part2.name}
+        exercises2={part2.exercises}
+        part3={part3.name}
+        exercises3={part3.exercises}
+      />
       <Total total= {part1.exercises + part2.exercises + part3.exercises} />
     </div>
   )
